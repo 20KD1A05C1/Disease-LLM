4dc2ab10e84f7d466fe3e0ece6bbb0df5fea304e
20KD1A05C1 <113783589+20KD1A05C1@users.noreply.github.com>
2024-10-16 12:44:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,7 +146,7 @@ if prompt := st.chat_input("What symptoms are you experiencing?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
      # Generate Cypher query
     cypher_query ="""
-MATCH (s:Symptom)-[:INDICATES]->(d:Disease)-[:TREATED_BY]->(m:Medicine) WHERE s.name IN ['fever', 'cough', 'fatigue'] RETURN d.name AS Disease, collect(s.name) AS Symptoms, collect(m.name) AS Medicines LIMIT 5
+MATCH (s:Symptom)-[:INDICATES]->(d:Disease)-[:TREATED_BY]->(m:Medicine) WHERE s.name IN ['Fever, cough, fatigue'] RETURN d.name AS Disease, collect(s.name) AS Symptoms, collect(m.name) AS Medicines LIMIT 5
 """
  #generate_cypher_query(prompt).strip("'")
     #st.write("tejas last" + cypher_query)
